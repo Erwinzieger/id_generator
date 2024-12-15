02d0e68d18ceb4ebfccf4be7d281fc422a954b85
Erwin Zieger <e-zieger@hotmail.com>
2024-12-15 19:42:35 -0300

    Update generador_de_id_unico.py

--- generador_de_id_unico.py
+++ generador_de_id_unico.py
@@ -1,6 +1,5 @@
 import random
 from random import randint
-from tkinter.font import names
 
 nombre = input('Escribe tu nombre: ')
 apellido = input('Escribe tu apellido: ')
@@ -11,13 +10,13 @@ aleatoreo_3 = randint(1,9)
 aleatoreo_4 = randint(1,9)
 
 
-print()  # Creamos un espacio
+print()  
 
 
 print(f'Hola {nombre} !')
-nombre = nombre[:2].upper()
-apellido = apellido[:2].upper()
-nacimiento = str(nacimiento)[2:]
+nombre = nombre[:2].upper()  # Primeros 2 digitos y en mayuscula
+apellido = apellido[:2].upper()  # Primeros 2 digitos y en mayuscula
+nacimiento = str(nacimiento)[2:]  # Ultimos 2 digitos
 
 print(' Tu numero de identificacion ID generado por el sistema es: ')
 print(f'{nombre}{apellido}{nacimiento}{aleatoreo_1}{aleatoreo_2}{aleatoreo_3}{aleatoreo_4}')
